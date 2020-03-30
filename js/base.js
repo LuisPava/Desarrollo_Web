739274a81b7fbd2658318ee482ec595e678c3525
Luis Pava <ingsislualpaca@gmail.com>
2020-03-30 01:37:55 -0500

    Proyecto Actualizado...

--- js/base.js
+++ js/base.js
@@ -1,3 +1,26 @@
 jQuery(document).ready(function () {
-
+  $("#btnRegistrar").click(function () {
+    //alert("Aquí Vamos ...");
+    document.getElementById("txtHora").value=Date().substring(16,25);
+  });
 });
+
+
+
+function horaActual() {
+  setTimeout(horaActual, 1000);
+  //alert(Date().substring(16,25));
+  document.getElementById("txtHora").value=Date().substring(16,25);
+  document.getElementsByName("phora").value="Salida";
+  $("#phora").text(Date().substring(16,25));
+  //document.getElementById("txtUsuario").innerHTML=Date().substring(16,25);
+}
+
+function horaIndex() {
+  setTimeout(horaIndex, 1000);
+  //alert(Date().substring(16,25));
+  document.getElementById("phora").innerHTML=Date().substring(16,25);
+  document.getElementsByName("phora").value=Date().substring(16,25);
+  //$("#phora").text(Date().substring(16,25));
+  //document.getElementById("txtUsuario").innerHTML=Date().substring(16,25);
+}
